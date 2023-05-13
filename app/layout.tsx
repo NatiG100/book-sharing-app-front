425efdd5aa3e9c56aa82d25895d71b6101a80825
NatiG100 <natnaelgashu2022@gmail.com>
2023-05-13 10:48:46 +0300

    conditional layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import HeroSection from '@/components/Sections/Hero'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AppBar from '@/components/AppBar'
+import FooterSection from '@/components/Sections/Footer'
+import HeaderBackdrop from '@/components/HeaderBackdrop'
+import {usePathname} from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +20,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathName = usePathname();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className='w-full '>
+          <div className={`w-full relative ${pathName==="/"?"h-screen":"h-[150px]"}`}>
+            {pathName==="/"?<HeroSection/>:<HeaderBackdrop/>}
+            <AppBar/>
+          </div>
+          {children}
+          <FooterSection/>
+        </div>
+      </body>
     </html>
   )
 }
