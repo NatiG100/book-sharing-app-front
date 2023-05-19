875c52fb972263098431105303841e3b59715a6e
NatiGK <natnaelgashu2022@gmail.com>
2023-05-19 10:31:12 +0300

    create strapi book types

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,61 @@
+export interface TypeStrapiBook{
+    id:number,
+    attributes:{
+        title:string,
+        pages:number,
+        pubDate:string,
+        description:string,
+        downloads:number,
+        view:number,
+        author:{
+            data:{
+                id:number,
+                attributes:{
+                    fullName:string
+                }
+            }
+        },
+        category:{
+            data:{
+                id:number,
+                attributes:{
+                    string:string
+                }
+            }
+        },
+        file:{
+            data:{
+                id:number,
+                attributes:{
+                    url:string
+                }
+            }
+        },
+        coverImg:{
+            data:{
+                id:number,
+                attributes:{
+                    formats:{
+                        thumbnail:{url:string},
+                        small:{url:string},
+                        medium:{url:string},
+                    },
+                    url:string
+                }
+            }
+        },
+    }
+};
+
+export interface TypeBookStrapiRes{
+    data:TypeStrapiBook,
+}
+export interface TypeBOoksStrapiRes{
+    data:TypeStrapiBook[],
+    meta:TypeMeta,
+}
+
+
 export interface TypeBook{
     id:number,
     title:string,
