c1fee49bb2aa1402e2e40a0f1b9f4a7b90567570
NatiGK <natnaelgashu2022@gmail.com>
2023-05-19 10:05:16 +0300

    fetch categories

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,12 +1,9 @@
 import BookList from "@/components/BookList";
 import ToggleChip from "@/components/UIElements/ToggleChip";
-import { API_MEILISEARCH } from "@/constants/API_URL";
+import { API_MEILISEARCH, API_URL } from "@/constants/API_URL";
 import { books } from "@/data";
-import { TypeBooksResponse } from "@/types/types";
-
-interface BooksProps{
-    searchParams:{query:string}
-}
+import { TypeBooksResponse, TypeCategoriesStrapiRes } from "@/types/types";
+import Link from "next/link";
 
 async function getBooks(query:string){
     const res = await fetch(`${API_MEILISEARCH}/indexes/book/search?q=${query||""}`,{
@@ -18,16 +15,43 @@ async function getBooks(query:string){
     }
     return res.json();
 }
+async function getCategories(){
+    const res = await fetch(`${API_URL}/api/categories`,{
+        method:"GET",
+        next:{revalidate:1000}
+    });
+    if(!res.ok){
+        throw new Error("Failed to fetch categories");
+    }
+    return res.json();
+}
+interface BooksProps{
+    searchParams:{query:string,category:string}
+}
+
 export default async function Books(props:BooksProps) {
     const bookList = await getBooks(props.searchParams.query);
+    const categoryList:TypeCategoriesStrapiRes = await getCategories();
     return (
         <>
             <div className="w-full max-w-[1000px] ml-auto mr-auto px-11 sm:px-20 lg:px-5 flex justify-start items-center gap-3 flex-wrap">
-                <ToggleChip isOn={true}>All</ToggleChip>
-                <ToggleChip isOn={false}>Doctrine</ToggleChip>
-                <ToggleChip isOn={false}>Practical</ToggleChip>
-                <ToggleChip isOn={false}>History</ToggleChip>
-                <ToggleChip isOn={false}>Others</ToggleChip>
+                <Link href={`/books?query=${props.searchParams.query}`}>
+                    <ToggleChip isOn={(function(){
+                        let isAll = true;
+                        categoryList.data.forEach((data)=>{
+                            if(data.attributes.string===props.searchParams.category){
+                                isAll = false;
+                                return;
+                            }
+                        })
+                        return isAll;
+                    })()}>All</ToggleChip>
+                </Link>
+                {categoryList.data.map((data)=>(
+                    <Link key={data.id} href={`/books?query=${props.searchParams.query||""}&category=${data.attributes.string}`}>
+                        <ToggleChip isOn={props.searchParams.category===data.attributes.string} >{data.attributes.string}</ToggleChip>
+                    </Link>
+                ))}
             </div>
             <BookList 
                 books={bookList.hits} 
