d134489cbb19067244d1ac39d2880b2d852bcc9e
NatiGK <natnaelgashu2022@gmail.com>
2023-05-22 10:15:03 +0300

    fetch popular and recently uploaded books

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
 import BookList from '@/components/BookList'
+import { API_MEILISEARCH } from '@/constants/API_URL';
 import { books } from '@/data'
+import { TypeBooksResponse } from '@/types/types';
 
-export default function Home() {
+async function getPopularBooks(){
+  const res = await fetch(`${API_MEILISEARCH}/indexes/book/search?sort=downloads:desc,view:desc&limit=8`,{
+      method:"GET",
+      next:{revalidate:0},
+  });
+  if(!res.ok){
+    throw new Error('Failed to fetch books.');
+  }
+  return res.json();
+}
+async function getRecentBooks(){
+  const res = await fetch(`${API_MEILISEARCH}/indexes/book/search?sort=createdAt:asc&limit=8`,{
+      method:"GET",
+      next:{revalidate:0},
+  });
+  if(!res.ok){
+    throw new Error('Failed to fetch books.');
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const popularBooks:TypeBooksResponse = await getPopularBooks();
+  const recentBooks:TypeBooksResponse = await getRecentBooks();
   return (
       <>
-        <BookList books={books} title='popular' className='mt-20'/>
-        <BookList books={books} title='recently uploaded' className='mt-20'/>
+        <BookList books={popularBooks.hits} title='popular' className='mt-20'/>
+        <BookList books={recentBooks.hits} title='recently uploaded' className='mt-20'/>
       </>     
   )
 }
